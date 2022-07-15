58962c5f14b33f9b9a4af61dcb88bc4804ea9323
Bruno Mauro <brunomtb08@gmail.com>
2022-07-15 09:31:56 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let musicas = [
+    {titulo:'Maria fumaça', artistas:'Banda black rio', src:'musicas/maria-fumaça.mp3', img:'imagens/maria-fumaça.jpg'},
+    {titulo:'Jimmy recard', artistas:'Drapht', src:'musicas/The Passenger.mp3', img:'imagens/jimmy-recard.jpg'},
+    {titulo:'Praise you', artistas:'Fatboy slim', src:'musicas/praise-you.mp3', img:'imagens/praise-you.jpg'}
+];
+
 let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
 
 //Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
@@ -7,7 +21,33 @@ document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
 //Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artistas;
+        imagem.src = musicas[index].img;    
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
 function tocarMusica(){
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
